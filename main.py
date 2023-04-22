c6213079c3bc3450d8a3ed1533389de2be1afcca
Jonas <sturmente@proton.me>
2023-04-22 17:38:16 +0200

    Connect to wifi

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from machine import Pin, I2C
 import utime as time
 from dht import DHT11, InvalidChecksum
+import network
+import secrets
+import time
 
-pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
-sensor = DHT11(pin)
-
+# Meassure function
 def meassure():
     try:
         return ((sensor.temperature), (sensor.humidity))
     except:
         return 
+
+pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+sensor = DHT11(pin)
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(secrets.SSID, secrets.PASSWORD)
+
+while not wlan.isconnected():
+    time.sleep(1)
 
 while True:
     m = meassure()
